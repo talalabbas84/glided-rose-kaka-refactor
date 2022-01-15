6399919bd76732bac13e9c32823d73e9c0af5476
Talal <talalabbas84@gmail.com>
2022-01-15 05:31:16 +0500

    implemented backstage class and handled all cases for sell in

---
+++ src/items/BackstagePasses.js
@@ -0,0 +1,16 @@
+import StandardItem from "./StandardItem";
+
+class BackStagePass extends StandardItem {
+  _calculateDepreciation() {
+    switch (true) {
+      case this.sellIn <= 0:
+        return this.quality;
+      case this.sellIn <= 5:
+        return -3;
+      case this.sellIn <= 10:
+        return -2;
+      default:
+        return -1;
+    }
+  }
+}
